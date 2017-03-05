8bbba9cc5825427619a54f76daa24a2d12c57699
Anton Davydov <antondavydov.o@gmail.com>
2017-03-06 00:45:44 +0300

    Simple refactoring

--- lib/rom_sql_graph/edges.rb
+++ lib/rom_sql_graph/edges.rb
@@ -7,12 +7,9 @@ module RomSqlGraph
     end
 
     def to_a
-      relations.map do |name, relation|
-        relation.schema.associations.elements.map do |_, object|
-          association_name = Inflecto.pluralize(object.target.to_sym.to_s).to_sym
-          [name, association_name]
-        end
-      end.map! { |array| array.empty? ? nil : array }.compact.flatten(1)
+      relations
+        .map { |name, relation| relation_associations(name, relation) }
+        .map! { |array| array.empty? ? nil : array }.compact.flatten(1)
     end
 
   private
@@ -22,6 +19,13 @@ module RomSqlGraph
     def sql_relations(repo)
       repo.container.relations.elements
         .select { |name, object| object.class.adapter == SQL }
+    end
+
+    def relation_associations(name, relation)
+      relation.schema.associations.elements.map do |_, object|
+        association_name = Inflecto.pluralize(object.target.to_sym.to_s).to_sym
+        [name, association_name]
+      end
     end
   end
 end
