7fce26421293970f5faf85f16d22defa00449ff8
Anton Davydov <antondavydov.o@gmail.com>
2020-05-06 22:26:24 +0300

    Generate simple ast related to db schema

--- lib/rom_sql_graph/edges.rb
+++ lib/rom_sql_graph/edges.rb
@@ -5,6 +5,9 @@ module RomSqlGraph
     def initialize(rom_container)
       @relations = sql_relations(rom_container)
       @table_names = @relations.keys
+      @schema = db_schema(@relations)
+
+      pp @schema
     end
 
     def to_a
@@ -20,8 +23,42 @@ module RomSqlGraph
 
     def sql_relations(container)
       container.relations.elements
-        .select { |name, object| object.class.adapter == SQL }
+        .select { |_table_name, object| object.class.adapter == SQL }
     end 
+
+    def db_schema(relations)
+      relations.map do |_table, relation|
+        schema = relation.schema
+
+        {
+          table_name: schema.name.to_s,
+          columns: schema.attributes.map do |attr|
+            {
+              name: attr.name.to_s,
+              type: attr.options[:type].to_ast
+              # **type_and_index(attr.options[:type])
+            }
+          end
+        }
+      end
+
+    end
+
+    def type_and_index(type)
+      if type.respond_to?(:primitive)
+        {
+          type: type.primitive,
+          index: type.meta[:index].to_a
+        }
+      else
+        {
+          type: type.meta[:db_type] || "#{type.left.type.primitive} | #{type.right.primitive}",
+          # type: type.meta[:db_type],
+          index: []
+        }
+      end
+
+    end
 
     def relation_associations(name, relation)
       relation.schema.associations.elements.map do |_, object|
